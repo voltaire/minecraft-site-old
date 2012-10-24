98ba58275b83c8ebef455f377eccd550e6392fea
Jon Chen <fly@sjchen.net>
2012-10-24 02:23:25 -0400

    add images

---
+++ js/indexbackstretch.js
@@ -0,0 +1,19 @@
+var images = [
+    "http://mc.voltaire.sh/i/bg1.png",
+    "http://mc.voltaire.sh/i/nightmountain.png",
+    "http://mc.voltaire.sh/i/nokbarmountain.png",
+    "http://mc.voltaire.sh/i/thunderbase.png"
+];
+        
+$(images).each(function(){
+    $('<img/>')[0].src = this; 
+});
+        
+var index = 0;
+        
+$.backstretch(images[index], {speed: 500});
+        
+setInterval(function() {
+    index = (index >= images.length - 1) ? 0 : index + 1;
+    $.backstretch(images[index]);
+}, 5000);
