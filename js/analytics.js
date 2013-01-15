186a5f3a124524429d97e6f4c07ea90d1f162d4c
Jon Chen <fly@sjchen.net>
2013-01-15 15:23:21 +0000

    use piwik instead

--- js/analytics.js
+++ js/analytics.js
@@ -1,3 +1,4 @@
+<!-- Google
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-35612802-1']);
 _gaq.push(['_trackPageview']);
@@ -6,4 +7,18 @@ _gaq.push(['_trackPageview']);
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+})(); -->
+
+
+<!-- Piwik --> 
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.sjchen.net/" : "http://stats.sjchen.net/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+    var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 5);
+    piwikTracker.trackPageView();
+    piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://stats.sjchen.net/piwik.php?idsite=5" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
