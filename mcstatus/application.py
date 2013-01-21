cd0b32b6d98d44be8badf0a8f0e1459ebc23f905
Jon Chen <fly@sjchen.net>
2013-01-21 21:00:53 +0000

    change to string

--- mcstatus/application.py
+++ mcstatus/application.py
@@ -10,7 +10,7 @@ def returnStatus():
     all_status = query.get_rules()
     server_info = 'The server has %d / %d players.' % (basic_status['numplayers'], basic_status['maxplayers'])
     status_info = 'Online now: %s' % (all_status['players'])
-    return "<pre>" + server_info + "\n" + status_info + "</pre>"
+    return "<pre>" + str(server_info) + "\n" + str(status_info) + "</pre>"
 
 if __name__ == '__main__':
     app.run()
