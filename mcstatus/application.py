887b12de2384c0184cf128640b9dbabbfb70a126
Jon Chen <fly@sjchen.net>
2013-02-15 04:06:49 +0000

    prettify online now

--- mcstatus/application.py
+++ mcstatus/application.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from minecraft_query import MinecraftQuery
 
 app = Flask(__name__)
@@ -9,8 +10,8 @@ def returnStatus():
     basic_status = query.get_status()
     all_status = query.get_rules()
     server_info = 'The server has %d / %d players.' % (basic_status['numplayers'], basic_status['maxplayers'])
-    status_info = 'Online now: %s' % (all_status['players'])
-    return "<pre>" + str(server_info) + "\n" + str(status_info) + "</pre>"
+    vanillaPlayers = MinecraftQuery("mc.voltaire.sh",25565).get_rules()['players']
+    return render_template('derp.html', avvys=vanillaPlayers)
 
 if __name__ == '__main__':
     app.run()
