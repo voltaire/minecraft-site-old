ff939b8f8ffc68f1fcd89fd8b9de7a3aa085df39
Jon Chen <fly@sjchen.net>
2013-02-15 03:24:02 +0000

    refresh every 5 minutes instead of 15 seconds

--- js/mcstatus.js
+++ js/mcstatus.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $('.mcstatus').load('http://mc.voltaire.sh/mcstatus');
     var refreshId = setInterval(function() {
         $('.mcstatus').load('http://mc.voltaire.sh/mcstatus');
-    }, 15000);
+    }, 300000);
     $.ajaxSetup({ cache: false });
 });
