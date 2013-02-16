7dcc9b2423d6319c5cba91096b6dfb68c7d1ce3d
Jon Chen <fly@sjchen.net>
2013-02-16 02:11:05 +0000

    clean up template

--- mcstatus/application.py
+++ mcstatus/application.py
@@ -9,9 +9,8 @@ def returnStatus():
     query = MinecraftQuery("mc.voltaire.sh", 25565)
     basic_status = query.get_status()
     all_status = query.get_rules()
-    server_info = 'The server has %d / %d players.' % (basic_status['numplayers'], basic_status['maxplayers'])
     vanillaPlayers = MinecraftQuery("mc.voltaire.sh",25565).get_rules()['players']
-    return render_template('derp.html', avatars=vanillaPlayers)
+    return render_template('derp.html', avatars=vanillaPlayers, numplayers=basic_status['numplayers'], maxplayers=basic_status['maxplayers'])
 
 if __name__ == '__main__':
     app.run()
